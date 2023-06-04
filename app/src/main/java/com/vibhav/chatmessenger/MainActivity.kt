75ef805716dc2d264db534075b41f1095ef94a86
Vibhav <vibhavsachan743@gmail.com>
2023-06-04 23:16:04 +0530

    Firebase setup

--- app/src/main/java/com/vibhav/chatmessenger/MainActivity.kt
+++ app/src/main/java/com/vibhav/chatmessenger/MainActivity.kt
@@ -2,10 +2,31 @@ package com.vibhav.chatmessenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.vibhav.chatmessenger.Adapter.ViewPagerAdapter
+import com.vibhav.chatmessenger.Fragments.CallFragment
+import com.vibhav.chatmessenger.Fragments.ChatFragment
+import com.vibhav.chatmessenger.Fragments.StatusFragment
+import com.vibhav.chatmessenger.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding : ActivityMainBinding? =null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        val fragmentArrayList = ArrayList<Fragment>()
+
+        fragmentArrayList.add(ChatFragment())
+        fragmentArrayList.add(StatusFragment())
+        fragmentArrayList.add(CallFragment())
+
+        val adapter = ViewPagerAdapter(this, supportFragmentManager, fragmentArrayList)
+
+        binding!!.viewPager.adapter = adapter
+
+        binding!!.tabs.setupWithViewPager(binding!!.viewPager)
     }
 }
